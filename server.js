1af6e48321e773515b814062a066133e753a2da0
DanielGambaO <dangamba99@gmail.com>
2024-04-28 14:37:20 -0700

    server

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const authRoutes = require("./routes/authRoutes");
+const recipeRoutes = require("./routes/recipeRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB using Mongoose
+app.use(bodyParser.json());
+
 mongoose
   .connect("mongodb://localhost:27017/recipeApp", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Error connecting to MongoDB:", err));
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-// Define your routes for CRUD operations on recipes, ratings, and comments
+app.use("/auth", authRoutes);
+app.use("/recipes", recipeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
